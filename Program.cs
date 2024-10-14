3bfe3df06d711baf5b267ea7f1890540026746bd
FLP013 <felliperibeiro9@gmail.com>
2024-10-14 13:00:54 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string Digitacao;
+
+Console.Clear();
+
+Console.WriteLine("--- PRIMEIRO CARCTERE ---");
+
+Console.Write("Digite algo: ");
+Digitacao = Console.ReadLine()!;
+
+Console.WriteLine(Digitacao.Trim().Substring(0,1));
